91206368148db95cd575fdff6be5f19c508faeca
VovAragok <vovgen9572@gmail.com>
2022-02-09 17:20:10 +0300

    add 1111

---
+++ 11111.py
@@ -0,0 +1,19 @@
+a = str(input("Хто? "))
+b = str(input("Дэ? "))
+c = str(input("Коли? "))
+print(f"{a} {b} {c}")
+
+# day = int(input("введите количество дней "))
+# hour = day * 24
+# minutes = hour * 60
+# sec = minutes * 60
+# print(f"Князь гвидон проплавав в дiжцi {hour} годин {minutes} хвилин, {sec} ceкунд")
+#
+# a = int(input("введите количество яиц "))
+# b = a + 2
+# c = b + 2
+# d = c + 2
+# e = d + 2
+# result = a + b + c + d + e
+# print(f" курица снесла {result} яиц")
+
